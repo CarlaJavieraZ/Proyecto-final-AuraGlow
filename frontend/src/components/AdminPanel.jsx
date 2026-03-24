779bbf1be5b81fbee6a09bf38213e32fb3c9ecb7
CarlaJavieraZ <carla.zuniga1995@gmail.com>
2026-03-23 23:22:37 -0300

    Update AdminPanel.jsx

--- frontend/src/components/AdminPanel.jsx
+++ frontend/src/components/AdminPanel.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://proyecto-final-auraglow.onrender.com";
 
 
 const AdminPanel = () => {
